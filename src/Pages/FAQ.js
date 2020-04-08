c6dd20f482592b339f620304aac130532ac855b7
mkiki3 <kikisymon3@gmail.com>
2020-04-07 19:41:35 -0500

    ngrok webhook test pls work

--- src/Pages/FAQ.js
+++ src/Pages/FAQ.js
@@ -23,7 +23,8 @@ class App extends Component {
     constructor(props){
         super(props);
         this.state = {  
-         email: " "
+         email: " ",
+         name: ""
         };
     }
 
